bbbe108c31649e48f04b3c3434ec5f5009eced0a
SilanSurfer <szwarc.adam@gmail.com>
2021-01-28 20:38:24 +0100

    style: Applied cargo fmt on all files in src

--- src/arg.rs
+++ src/arg.rs
@@ -1,7 +1,5 @@
-
 extern crate clap;
-use clap::{App, Arg, SubCommand, ArgMatches};
-
+use clap::{App, Arg, ArgMatches, SubCommand};
 
 pub enum PngMeCliArgs {
     Encode(EncodeArgs),
@@ -33,61 +31,51 @@ pub struct PrintArgs {
 
 pub fn get_cli_args<'a>() -> ArgMatches<'a> {
     let matches = App::new("PNGme")
-    .version("0.1.0")
-    .author("SilanSurfer <szwarc.adam@gmail.com>")
-    .about("Let's you modify PNG file to include secret message")
-    .subcommand(SubCommand::with_name("encode")
-        .about("Encodes string in PNG file")
-        .arg(
-            Arg::with_name("filename")
-                .help("Name of the file to encode message")
-                .required(true),
+        .version("0.1.0")
+        .author("SilanSurfer <szwarc.adam@gmail.com>")
+        .about("Let's you modify PNG file to include secret message")
+        .subcommand(
+            SubCommand::with_name("encode")
+                .about("Encodes string in PNG file")
+                .arg(
+                    Arg::with_name("filename")
+                        .help("Name of the file to encode message")
+                        .required(true),
+                )
+                .arg(Arg::with_name("type").help("Chunk type").required(true))
+                .arg(
+                    Arg::with_name("message")
+                        .help("Message to encode")
+                        .default_value(""),
+                ),
         )
-        .arg(
-            Arg::with_name("type")
-                .help("Chunk type")
-                .required(true)
+        .subcommand(
+            SubCommand::with_name("decode")
+                .about("Decodes chunk with type provided")
+                .arg(
+                    Arg::with_name("filename")
+                        .help("Name of the file to encode message")
+                        .required(true),
+                )
+                .arg(Arg::with_name("type").help("Chunk type").required(true)),
         )
-        .arg(
-            Arg::with_name("message")
-                .help("Message to encode")
-                .default_value("")
+        .subcommand(
+            SubCommand::with_name("remove")
+                .about("Removes chunk with type provided")
+                .arg(
+                    Arg::with_name("filename")
+                        .help("Name of the file to encode message")
+                        .required(true),
+                )
+                .arg(Arg::with_name("type").help("Chunk type").required(true)),
         )
-    )
-    .subcommand(SubCommand::with_name("decode")
-        .about("Decodes chunk with type provided")
-        .arg(
-            Arg::with_name("filename")
-                .help("Name of the file to encode message")
-                .required(true),
+        .subcommand(
+            SubCommand::with_name("print").about("Prints PNG file").arg(
+                Arg::with_name("filename")
+                    .help("Name of the file to encode message")
+                    .required(true),
+            ),
         )
-        .arg(
-            Arg::with_name("type")
-                .help("Chunk type")
-                .required(true)
-        )
-    )
-    .subcommand(SubCommand::with_name("remove")
-        .about("Removes chunk with type provided")
-        .arg(
-            Arg::with_name("filename")
-                .help("Name of the file to encode message")
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("type")
-                .help("Chunk type")
-                .required(true)
-        )
-    )
-    .subcommand(SubCommand::with_name("print")
-        .about("Prints PNG file")
-        .arg(
-            Arg::with_name("filename")
-                .help("Name of the file to encode message")
-                .required(true),
-        )
-    )
-    .get_matches();
+        .get_matches();
     return matches;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
+mod arg;
 mod chunk;
 mod chunk_type;
 mod error;
 mod png;
-mod arg;
 
-use crate::arg::{get_cli_args, PngMeCliArgs, EncodeArgs, DecodeArgs, PrintArgs, RemoveArgs};
+use crate::arg::{get_cli_args, DecodeArgs, EncodeArgs, PngMeCliArgs, PrintArgs, RemoveArgs};
 
 fn main() -> Result<(), error::PngMeError> {
     let matches = get_cli_args();
 
     let args = match matches.subcommand() {
-        ("encode", Some(encode_args)) => Ok(PngMeCliArgs::Encode(EncodeArgs{
+        ("encode", Some(encode_args)) => Ok(PngMeCliArgs::Encode(EncodeArgs {
             filename: encode_args.value_of("filename").unwrap().to_string(),
             chunk_type: encode_args.value_of("type").unwrap().to_string(),
             msg: encode_args.value_of("message").unwrap().to_string(),
             output_file: None,
         })),
-        ("decode", Some(decode_args)) => Ok(PngMeCliArgs::Decode(DecodeArgs{
+        ("decode", Some(decode_args)) => Ok(PngMeCliArgs::Decode(DecodeArgs {
             filename: decode_args.value_of("filename").unwrap().to_string(),
             chunk_type: decode_args.value_of("type").unwrap().to_string(),
         })),
-        ("remove", Some(remove_args)) => Ok(PngMeCliArgs::Remove(RemoveArgs{
+        ("remove", Some(remove_args)) => Ok(PngMeCliArgs::Remove(RemoveArgs {
             filename: remove_args.value_of("filename").unwrap().to_string(),
             chunk_type: remove_args.value_of("type").unwrap().to_string(),
         })),
-        ("print", Some(print_args)) => Ok(PngMeCliArgs::Print(PrintArgs{
+        ("print", Some(print_args)) => Ok(PngMeCliArgs::Print(PrintArgs {
             filename: print_args.value_of("filename").unwrap().to_string(),
         })),
         (_, _) => Err(error::PngMeError::InvalidCliArguments),
